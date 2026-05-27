6a1dc1b7bab48feb982ee67e71dab90a3d6e9271
Javier <javier@eskura21.com>
2026-05-27 09:00:26 +0200

    Refactorización

--- FRONTEND/JAVASCRIPT/js/tareas.js
+++ FRONTEND/JAVASCRIPT/js/tareas.js
@@ -2,46 +2,50 @@
 
 window.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
-    const ul = document.querySelector('ul');
-
-    form.addEventListener('submit', e => {
-        e.preventDefault();
-
-        const li = document.createElement('li');
-        const input = document.createElement('input');
-        const label = document.createElement('label');
-
-        const textoTarea = form.tarea.value.trim();
-        
-        if(!textoTarea) {
-            return;
-        }
-
-        input.type = 'checkbox';
-        input.addEventListener('change', e => {
-            const elInput = e.target;
-            const laLabel = elInput.parentNode;
-            
-            console.log(elInput);
-            console.log(laLabel);
-
-            console.log(elInput.checked);
-
-            if(elInput.checked) {
-                laLabel.classList.add('completada');
-            } else {
-                laLabel.classList.remove('completada');
-            }
-        });
-
-        label.textContent = textoTarea;
-
-        label.appendChild(input);
-
-        li.appendChild(label);
-
-        ul.appendChild(li);
-
-        form.reset();
-    });
+    
+    form.addEventListener('submit', agregarTarea);
 });
+
+function agregarTarea(e) {
+    e.preventDefault();
+    
+    const form = e.target;
+    const textoTarea = form.tarea.value.trim();
+    
+    if (!textoTarea) {
+        return;
+    }
+
+    const ul = document.querySelector('ul');
+    
+    const li = document.createElement('li');
+    const label = document.createElement('label');
+    const input = document.createElement('input');
+
+    input.type = 'checkbox';
+    input.addEventListener('change', clickTarea);
+
+    label.textContent = textoTarea;
+
+    label.appendChild(input);
+    li.appendChild(label);
+    ul.appendChild(li);
+
+    form.reset();
+}
+
+function clickTarea(e) {
+    const elInput = e.target;
+    const laLabel = elInput.parentNode;
+
+    console.log(elInput);
+    console.log(laLabel);
+
+    console.log(elInput.checked);
+
+    if (elInput.checked) {
+        laLabel.classList.add('completada');
+    } else {
+        laLabel.classList.remove('completada');
+    }
+}
