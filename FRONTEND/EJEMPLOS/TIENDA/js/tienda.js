5e0e553ceea8e43586274a3849851caf36719fe1
Javier <javier@eskura21.com>
2026-06-03 11:01:06 +0200

    Formulario de búsqueda funcional

--- FRONTEND/EJEMPLOS/TIENDA/js/tienda.js
+++ FRONTEND/EJEMPLOS/TIENDA/js/tienda.js
@@ -2,17 +2,52 @@ const URL_PRODUCTOS = 'https://6a1fe16be96c1d13b5868fb0.mockapi.io/api/v1/produc
 
 const listadoProductos = document.querySelector('#productos div');
 
-const respuesta = await fetch(URL_PRODUCTOS);
-const productos = await respuesta.json();
+await listarProductos();
 
-for (const p of productos) {
-    const div = document.createElement('div');
+const formularioBusqueda = document.querySelector('#formulario-busqueda');
 
-    div.className = 'col';
+formularioBusqueda.addEventListener('submit', e => {
+    e.preventDefault();
 
-    const htmlProducto = `
+    const texto = formularioBusqueda.texto.value;
+
+    formularioBusqueda.reset();
+
+    listarProductos(texto);
+});
+
+async function listarProductos(texto) {
+    let url = URL_PRODUCTOS;
+
+    if(texto) {
+        url += '?nombre=' + texto;
+    }
+
+    let respuesta = await fetch(url);
+
+    console.log(respuesta);
+
+    if(!respuesta.ok) {
+        alert('No se ha encontrado ningún producto con esas características');
+        respuesta = await fetch(URL_PRODUCTOS);
+    }
+
+    const productos = await respuesta.json();
+
+    console.log(productos);
+
+    listadoProductos.innerHTML = '';
+
+    for (const p of productos) {
+        console.log(p);
+
+        const div = document.createElement('div');
+
+        div.className = 'col';
+
+        const htmlProducto = `
         <div class="card h-100">
-            <img src="https://picsum.photos/id/${p.id}/400/300" class="card-img-top" alt="Cámara Canon EOS R10">
+            <img src="https://picsum.photos/id/${p.id}/400/300" class="card-img-top" alt="${p.nombre}">
             <div class="card-body">
                 <h5 class="card-title">${p.nombre}</h5>
                 <p class="card-text">${p.departamento}</p>
@@ -23,7 +58,8 @@ for (const p of productos) {
         </div>
     `;
 
-    div.innerHTML = htmlProducto;
+        div.innerHTML = htmlProducto;
 
-    listadoProductos.appendChild(div);
+        listadoProductos.appendChild(div);
+    }
 }
