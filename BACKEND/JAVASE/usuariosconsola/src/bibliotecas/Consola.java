569a18876250c1a07394d9b13200bc2dfeac2455
'Javier <'javier@eskura21.com'>
2026-06-30 09:53:28 +0200

    Mejoras de Consola

--- BACKEND/JAVASE/usuariosconsola/src/bibliotecas/Consola.java
+++ BACKEND/JAVASE/usuariosconsola/src/bibliotecas/Consola.java
@@ -6,8 +6,20 @@ import java.util.Scanner;
 public class Consola {
 	private static final Scanner SC = new Scanner(System.in);
 	
+	public static void pl(Object mensaje) {
+		System.out.println(mensaje);
+	}
+	
+	public static void p(Object mensaje) {
+		System.out.print(mensaje);
+	}
+	
+	public static void pf(String formato, Object... objetos) {
+		System.out.printf(formato, objetos);
+	}
+	
 	public static String pedirString(String mensaje) {
-		System.out.print(mensaje + ": ");
+		p(mensaje + ": ");
 		return SC.nextLine();
 	}
 
