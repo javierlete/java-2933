f6afdef7df44f4ae117827c713a1164c26000220
'Javier <'javier@eskura21.com'>
2026-07-03 08:36:39 +0200

    Copiamos el accesodatos y los dtos

---
+++ BACKEND/JAKARTAEE/basicosweb/src/main/java/accesodatos/RolCrud.java
@@ -0,0 +1,45 @@
+package accesodatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import dtos.Rol;
+
+public class RolCrud {
+	private static final String JDBC_URL = "jdbc:mysql://localhost:3306/amazonia";
+	private static final String JDBC_USER = "amazonia_app";
+	private static final String JDBC_PASS = "app";
+
+	private static final String SQL_SELECT = "SELECT * FROM roles";
+	
+	private static PreparedStatement crearSentencia(String sql) {
+		try {
+			Connection con = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
+			PreparedStatement pst = con.prepareStatement(sql);
+	
+			return pst;
+		} catch (SQLException e) {
+			throw new RuntimeException("No se ha podido conectar a la base de datos", e);
+		}
+	}
+
+	public static ArrayList<Rol> obtenerTodos() {
+		try (PreparedStatement pst = crearSentencia(SQL_SELECT); ResultSet rs = pst.executeQuery()) {
+			ArrayList<Rol> roles = new ArrayList<>();
+
+			while (rs.next()) {
+				Rol rol = new Rol(rs.getLong("id"), rs.getString("nombre"), rs.getString("descripcion"));
+
+				roles.add(rol);
+			}
+
+			return roles;
+		} catch (SQLException e) {
+			throw new RuntimeException("No se ha podido leer el listado", e);
+		}
+	}
+}
---
+++ BACKEND/JAKARTAEE/basicosweb/src/main/java/dtos/Rol.java
@@ -0,0 +1,5 @@
+package dtos;
+
+public record Rol(Long id, String nombre, String descripcion) {
+
+}
