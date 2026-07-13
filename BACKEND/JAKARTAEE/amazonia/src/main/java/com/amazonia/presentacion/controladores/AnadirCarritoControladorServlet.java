441ca7112d0898c9ddb8e82a367156b2ca985f44
'Javier <'javier@eskura21.com'>
2026-07-13 09:13:48 +0200

    En carrito al cambiar la cantidad de 1 a 0 borra línea

--- BACKEND/JAKARTAEE/amazonia/src/main/java/com/amazonia/presentacion/controladores/AnadirCarritoControladorServlet.java
+++ BACKEND/JAKARTAEE/amazonia/src/main/java/com/amazonia/presentacion/controladores/AnadirCarritoControladorServlet.java
@@ -18,48 +18,57 @@ import jakarta.servlet.http.HttpSession;
 @WebServlet("/carrito/anadir")
 public class AnadirCarritoControladorServlet extends HttpServlet {
 	private static final Logger log = Logger.getLogger(AnadirCarritoControladorServlet.class.getName());
-	
+
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// 1. Recibir información de la petición
 		HttpSession session = request.getSession();
-		
+
 		Carrito carrito = (Carrito) session.getAttribute("carrito");
-		
+
 		String sId = request.getParameter("id");
 		String sCantidad = request.getParameter("cantidad");
-		
+
 		// 2. Convertir los datos
 		Long id = Long.parseLong(sId);
 		Integer cantidad = Integer.parseInt(sCantidad);
-		
+
 		// 3. Crear un objeto con ellos
 		// 4. Llamar a la lógica de negocio
 		Producto producto = AnonimoNegocio.verDetalleProducto(id);
-		
+
 		log.info("Producto: " + producto);
-		
+
 		agregarProductoACarrito(carrito, producto, cantidad);
-		
+
 		// 5. Empaquetar la información para la siguiente vista
 		// 6. Saltar a la siguiente vista
 		response.sendRedirect(request.getContextPath() + "/carrito");
 	}
 
 	private Carrito agregarProductoACarrito(Carrito carrito, Producto producto, Integer cantidad) {
-		for(Linea linea: carrito.lineas()) {
-			if(linea.producto().id() == producto.id()) {
+		boolean insertarLinea = true;
+
+		for (Linea linea : carrito.lineas()) {
+			if (linea.producto().id() == producto.id()) {
 				cantidad += linea.cantidad();
-				
+
+				if (cantidad == 0) {
+					insertarLinea = false;
+				}
+
 				carrito.lineas().remove(linea);
-				
+
 				break;
 			}
 		}
-		
-		carrito.lineas().add(new Linea(producto, cantidad));
-		
+
+		if (insertarLinea) {
+			carrito.lineas().add(new Linea(producto, cantidad));
+		}
+
 		return carrito;
 	}
 
