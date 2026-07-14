ce3a2c329846feb5b682fbad8ca34be259f962b7
'Javier <'javier@eskura21.com'>
2026-07-14 11:12:00 +0200

    facturar: Corrección de los CRUDs para facturación REAL

--- BACKEND/JAKARTAEE/amazonia/src/main/java/com/amazonia/accesodatos/FacturaCrud.java
+++ BACKEND/JAKARTAEE/amazonia/src/main/java/com/amazonia/accesodatos/FacturaCrud.java
@@ -1,17 +1,132 @@
 package com.amazonia.accesodatos;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 import com.amazonia.dtos.Cliente;
 import com.amazonia.dtos.Factura;
+import com.amazonia.dtos.Factura.Linea;
+import com.amazonia.dtos.Producto;
+
+import bibliotecas.accesodatos.BaseDeDatos;
 
 public class FacturaCrud {
+	private static final String SQL_INSERT = """
+			INSERT INTO facturas (numero, fecha, clientes_id)
+			VALUES (
+			    (SELECT CONCAT(
+			        YEAR(CURRENT_DATE()),
+			        '-',
+			        LPAD(COALESCE(MAX(CAST(SUBSTRING_INDEX(numero, '-', -1) AS UNSIGNED)), 0) + 1, 4, '0')
+			    ) AS ultima_factura FROM facturas AS f WHERE numero LIKE CONCAT(YEAR(CURRENT_DATE()), '-%')),
+			    CURRENT_DATE(),
+			    ?
+			);
+
+			""";
+	private static final String SQL_INSERT_LINEA = """
+			INSERT INTO facturas_tiene_productos (facturas_id, productos_id, cantidad) VALUES (?,?,?);
+			""";
+
+	private static final String SQL_SELECT_ID = """
+			SELECT
+			    f.id AS f_id,
+			    f.numero AS f_numero,
+			    f.fecha AS f_fecha,
+			    c.id AS c_id,
+			    c.nombre AS c_nombre,
+			    c.apellidos AS c_apellidos,
+			    c.nif AS c_nif
+			FROM
+			    facturas f
+			        JOIN
+			    clientes c ON c.id = f.clientes_id
+			WHERE
+			    f.id = ?
+			""";
+
+	private static final String SQL_SELECT_LINEA = """
+			SELECT
+			    p.id AS p_id,
+			    p.nombre AS p_nombre,
+			    p.precio AS p_precio,
+			    p.descripcion AS p_descripcion,
+			    fp.cantidad AS fp_cantidad
+			FROM
+			    facturas_tiene_productos fp
+			        JOIN
+			    productos p ON p.id = fp.productos_id
+			WHERE
+			    facturas_id = ?
+			""";
+
+	public static Factura obtenerPorId(Long id) {
+		try (PreparedStatement pst = BaseDeDatos.crearSentencia(SQL_SELECT_ID)) {
+			pst.setLong(1, id);
+			ResultSet rs = pst.executeQuery();
+
+			Factura factura = null;
+
+			if (rs.next()) {
+				String facturaNumero = rs.getString("f_numero");
+				LocalDate facturaFecha = rs.getDate("f_fecha").toLocalDate();
+
+				Cliente cliente = new Cliente(rs.getLong("c_id"), rs.getString("c_nombre"), rs.getString("c_apellidos"),
+						rs.getString("c_nif"));
+
+				PreparedStatement pstLinea = BaseDeDatos.crearSentencia(SQL_SELECT_LINEA);
+
+				pstLinea.setLong(1, id);
+
+				ArrayList<Linea> lineas = new ArrayList<Linea>();
+
+				ResultSet rsLineas = pstLinea.executeQuery();
+
+				while (rsLineas.next()) {
+					Producto producto = new Producto(rsLineas.getLong("p_id"), rsLineas.getString("p_nombre"),
+							rsLineas.getString("p_descripcion"), rsLineas.getBigDecimal("p_precio"));
+					lineas.add(new Linea(producto, rsLineas.getInt("fp_cantidad")));
+				}
+				
+				factura = new Factura(id, facturaNumero, facturaFecha, cliente, lineas);
+			}
+
+			return factura;
+		} catch (SQLException e) {
+			throw new RuntimeException("Error al obtener la factura " + id, e);
+		}
+	}
 
 	public static Factura insertar(Factura facturaProvisional) {
-		Cliente cliente = new Cliente(2L, "Pepe", "Pérez", "12345678Z");
+		Long idCliente = facturaProvisional.cliente().id();
 
-		Factura factura = new Factura(1L, "2026-0002", facturaProvisional.fecha(), cliente,
-				facturaProvisional.lineas());
+		try (PreparedStatement pst = BaseDeDatos.crearSentencia(SQL_INSERT)) {
+			pst.setLong(1, idCliente);
+			pst.executeUpdate();
 
-		return factura;
+			ResultSet rs = pst.getGeneratedKeys();
+			rs.next();
+
+			Long idFactura = rs.getLong(1);
+
+			PreparedStatement pstLinea = BaseDeDatos.crearSentencia(SQL_INSERT_LINEA);
+			pstLinea.setLong(1, idFactura);
+
+			for (Linea linea : facturaProvisional.lineas()) {
+				pstLinea.setLong(2, linea.producto().id());
+				pstLinea.setInt(3, linea.cantidad());
+
+				pstLinea.executeUpdate();
+			}
+
+			return obtenerPorId(idFactura);
+		} catch (SQLException e) {
+			throw new RuntimeException("Error al guardar la factura " + facturaProvisional, e);
+		}
+
 	}
 
 }
--- BACKEND/JAKARTAEE/amazonia/src/main/java/com/amazonia/accesodatos/UsuarioCrud.java
+++ BACKEND/JAKARTAEE/amazonia/src/main/java/com/amazonia/accesodatos/UsuarioCrud.java
@@ -4,16 +4,31 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.amazonia.dtos.Cliente;
 import com.amazonia.dtos.Usuario;
 
 import bibliotecas.accesodatos.BaseDeDatos;
 
 public class UsuarioCrud {
 	private static final String SQL_SELECT_EMAIL = """
-			SELECT u.id AS u_id, u.email AS u_email, u.password AS u_password, u.nombre AS u_nombre, r.nombre AS r_nombre
-			FROM usuarios u
-			JOIN roles r ON r.id = u.roles_id
-			WHERE email=?
+			SELECT 
+			    u.id AS u_id,
+			    u.email AS u_email,
+			    u.password AS u_password,
+			    u.nombre AS u_nombre,
+			    r.nombre AS r_nombre,
+			    c.id AS c_id,
+			    c.nombre AS c_nombre,
+			    c.apellidos AS c_apellidos,
+			    c.nif AS c_nif
+			FROM
+			    usuarios u
+			        JOIN
+			    roles r ON r.id = u.roles_id
+					JOIN
+				clientes c ON c.id = u.clientes_id
+			WHERE
+			    email = ?
 			""";
 
 	public static Usuario obtenerPorEmail(String email) {
@@ -22,11 +37,11 @@ public class UsuarioCrud {
 			ResultSet rs = pst.executeQuery();
 
 			Usuario usuario = null;
-
+			
 			if (rs.next()) {
-				// TODO: Añadir los datos de cliente
+				Cliente cliente = new Cliente(rs.getLong("c_id"), rs.getString("c_nombre"), rs.getString("c_apellidos"), rs.getString("c_nif"));
 				usuario = new Usuario(rs.getLong("u_id"), rs.getString("u_nombre"), rs.getString("u_email"), rs.getString("u_password"),
-						rs.getString("r_nombre"), null);
+						rs.getString("r_nombre"), cliente);
 			}
 
 			return usuario;
