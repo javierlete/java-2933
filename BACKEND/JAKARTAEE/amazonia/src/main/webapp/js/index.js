566909984be12b947236cd065a428d46945a1081
'Javier <'javier@eskura21.com'>
2026-07-24 08:57:44 +0200

    Listado con aspecto Bootstrap en el frontend

--- BACKEND/JAKARTAEE/amazonia/src/main/webapp/js/index.js
+++ BACKEND/JAKARTAEE/amazonia/src/main/webapp/js/index.js
@@ -1,15 +1,42 @@
 const URL = 'api/v1/productos';
 
+function euro(cantidad) {
+    const fmt = new Intl.NumberFormat('es-ES', {
+        style: 'currency',
+        currency: 'EUR',
+        minimumFractionDigits: 2,
+		useGrouping: true,
+    });
+
+    return fmt.format(cantidad);
+}
+
 window.addEventListener('DOMContentLoaded', async () => {
-	const respuesta = await fetch(URL);
-	const productos = await respuesta.json();
-	
-	const ul = document.querySelector('#listado');
-	
-	for(const p of productos) {
-		const li = document.createElement('li');
-		li.innerHTML = `${p.nombre}: ${p.precio}`;
-		
-		ul.append(li);
-	}
+    const respuesta = await fetch(URL);
+    const productos = await respuesta.json();
+
+    const fila = document.querySelector('#listado .row');
+
+    for (const p of productos) {
+        const div = document.createElement('div');
+        div.className = 'col';
+        div.innerHTML = `
+			<div class="card h-100">
+				<img src="fotos/${p.id}.jpg" class="card-img-top" alt="...">
+				<div class="card-body">
+					<h5 class="card-title">${p.nombre}</h5>
+					<p class="card-text">${p.descripcion ?? ''}</p>
+					<p class="card-text">
+						<a class="btn btn-primary" href="detalle?id=${p.id}">Ver
+							producto</a>
+					</p>
+				</div>
+				<div class="card-footer">
+					<small class="text-body-secondary">${euro(p.precio)}</small>
+				</div>
+			</div>
+		`;
+
+        fila.append(div);
+    }
 });
